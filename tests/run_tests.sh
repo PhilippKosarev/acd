b759a11be255e6f3694c07715b52f625830ca4ba
PhilippKosarev <philipp.kosarev@gmail.com>
2025-12-31 21:21:08 +0000

    add library unit testing

---
+++ tests/run_tests.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+echo "Testing library:"
+python3 -m "unittest" "test_library.py"
---
+++ tests/test_library.py
@@ -0,0 +1,67 @@
+# Imports
+from pathlib import Path
+import unittest
+import tempfile
+import io
+import acd
+
+# Example variables
+example_file_name = 'example.acd'
+example_file = Path.cwd() / example_file_name
+example_encryption_key = '66-35-39-166-60-105-70-101'
+example_encrypted_data = b'\x0c\x00\x00\x00example-file\x15\x00\x00\x00\x9b\x00\x00\x00\xae\x00\x00\x00\x8e\x00\x00\x00\xa0\x00\x00\x00\xa5\x00\x00\x00\x99\x00\x00\x00\x98\x00\x00\x00Y\x00\x00\x00\x93\x00\x00\x00\x9a\x00\x00\x00\xa2\x00\x00\x00\x9b\x00\x00\x00M\x00\x00\x00\x99\x00\x00\x00\x9f\x00\x00\x00\x9b\x00\x00\x00\xa5\x00\x00\x00\x95\x00\x00\x00\xa3\x00\x00\x00\xa1\x00\x00\x00\xaa\x00\x00\x00'
+example_dict = {'example-file': 'example file contents'}
+
+# Testing if the example file exists.
+class TestFile(unittest.TestCase):
+  def test_example_file_exists(self):
+    self.assertTrue(example_file.is_file())
+
+  def test_example_file_data(self):
+    with open(example_file, 'rb') as file:
+      data = file.read()
+    self.assertEqual(data, example_encrypted_data)
+
+# Testing reading and writing acd file objects/bytes/files.
+class TestLibrary(unittest.TestCase):
+  def test_encyrption_key_generation(self):
+    encryption_key = acd.get_encryption_key_for_file(example_file)
+    self.assertEqual(encryption_key, example_encryption_key)
+    another_encryption_key = acd.get_encryption_key_for_string(example_file_name)
+    self.assertEqual(another_encryption_key, encryption_key)
+
+  def test_reading_from_file_object(self):
+    with open(example_file, 'rb') as file:
+      data = acd.read_from_file_object(file, example_encryption_key)
+    self.assertEqual(data, example_dict)
+
+  def test_reading_bytes(self):
+    data = acd.read_bytes(example_encrypted_data, example_encryption_key)
+    self.assertEqual(data, example_dict)
+    data = acd.read_bytes(bytearray(example_encrypted_data), example_encryption_key)
+    self.assertEqual(data, example_dict)
+
+  def test_reading_file(self):
+    data = acd.read_file(example_file)
+    self.assertEqual(data, example_dict)
+    data = acd.read_file(str(example_file))
+    self.assertEqual(data, example_dict)
+
+  def test_writing_to_file_object(self):
+    fp = io.BytesIO()
+    acd.write_to_file_object(fp, example_dict, example_encryption_key)
+    b = fp.getvalue()
+    self.assertEqual(b, example_encrypted_data)
+
+  def test_writing_to_bytes(self):
+    b = acd.write_bytes(example_dict, example_encryption_key)
+    self.assertEqual(b, example_encrypted_data)
+
+  def test_writing_to_file(self):
+    with tempfile.TemporaryDirectory() as tmp:
+      out_file = Path(tmp) / 'data.acd'
+      acd.write_file(out_file, example_dict)
+      data1 = acd.read_file(out_file)
+      data2 = acd.read_file(str(out_file))
+    self.assertEqual(data1, example_dict)
+    self.assertEqual(data2, example_dict)
