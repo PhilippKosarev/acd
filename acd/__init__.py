3d969b409637efd246cbf9cfbb8367a8735d909a
PhilippKosarev <philipp.kosarev@gmail.com>
2026-05-01 17:40:53 +0100

    remove unnecessary directory

--- acd/__init__.py
+++ acd/__init__.py
@@ -1,1 +1,10 @@
-from .acd import *
+from .acd import (
+  get_encryption_key_for_string,
+  get_encryption_key_for_file,
+  read,
+  write,
+  read_bytes,
+  write_bytes,
+  read_file,
+  write_file,
+)
