616ce5a88aa5d1a3a56514fc5909c56c7a9fa3de
PhilippKosarev <philipp.kosarev@gmail.com>
2026-06-19 20:04:01 +0100

    switch to pytest

---
+++ test_library.py
@@ -0,0 +1,67 @@
+# Imports
+from pathlib import Path
+import io
+import sys
+import tempfile
+
+import acd
+
+# Example data
+example_encryption_key = '192-45-0-55-66-241-55-117'
+example_data = {'example-file': 'example file contents'}
+example_data_encrypted = b'\x0c\x00\x00\x00example-file\x15\x00\x00\x00\x96\x00\x00\x00\xb1\x00\x00\x00\x93\x00\x00\x00\x9a\x00\x00\x00\xa4\x00\x00\x00\xa1\x00\x00\x00\x92\x00\x00\x00P\x00\x00\x00\x93\x00\x00\x00\x9e\x00\x00\x00\xa1\x00\x00\x00\x92\x00\x00\x00V\x00\x00\x00\x99\x00\x00\x00\x9c\x00\x00\x00\xa0\x00\x00\x00\xa8\x00\x00\x00\x96\x00\x00\x00\x9b\x00\x00\x00\xa9\x00\x00\x00\xa8\x00\x00\x00'
+
+# Tests
+def test_get_encryption_key_for_string():
+  encryption_key = acd.get_encryption_key_for_string('test')
+  assert encryption_key == example_encryption_key
+
+def test_get_encryption_key_for_file():
+  d = Path().cwd() / 'this' / 'is' / 'a' / 'test'
+  encryption_key = acd.get_encryption_key_for_file(d / 'data.acd')
+  assert encryption_key == example_encryption_key
+  encryption_key = acd.get_encryption_key_for_file(d / 'data')
+  assert encryption_key == example_encryption_key
+  encryption_key = acd.get_encryption_key_for_file(d / 'dAtA.bip.bop')
+  assert encryption_key == example_encryption_key
+  encryption_key = acd.get_encryption_key_for_file(d / 'duck.pond')
+  assert encryption_key == '134-78-80-97-174-1-25-101'
+
+def test_read():
+  fp = io.BytesIO(example_data_encrypted)
+  data = acd.read(fp, example_encryption_key)
+  assert data == example_data
+  assert not fp.closed
+
+def test_write():
+  fp = io.BytesIO()
+  acd.write(example_data, fp, example_encryption_key)
+  data = fp.getvalue()
+  assert data == example_data_encrypted
+  assert not fp.closed
+
+def test_read_bytes():
+  data = acd.read_bytes(example_data_encrypted, example_encryption_key)
+  assert data == example_data
+  encrypted_bytearray = bytearray(example_data_encrypted)
+  data = acd.read_bytes(encrypted_bytearray, example_encryption_key)
+  assert data == example_data
+
+def test_write_bytes():
+  data = acd.write_bytes(example_data, example_encryption_key)
+  assert data == example_data_encrypted
+
+def test_read_file():
+  with tempfile.TemporaryDirectory() as tmp:
+    file = Path(tmp) / 'test'
+    with open(file, 'wb') as fp:
+      fp.write(example_data_encrypted)
+    data = acd.read_file(file)
+  assert data == example_data
+
+def test_write_file():
+  with tempfile.TemporaryDirectory() as tmp:
+    file = Path(tmp) / 'test'
+    acd.write_file(example_data, file)
+    data = acd.read_file(file)
+  assert data == example_data
--- tests/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-script_dir="$(dirname "$0")"
-cd "$script_dir"
-
-echo "Testing library:"
-python3 -m "unittest" "test_library.py"
